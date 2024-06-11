c664dc526a28e5cfd4b009a3baf695821dd8f805
Brian Mai <0hrs3g@gmail.com>
2024-06-11 13:21:13 -0700

    Remove move scripts

--- scripts/board.js
+++ scripts/board.js
@@ -11,6 +11,8 @@ const board5x5 = document.getElementById("board-5x5");
 const board6x6 = document.getElementById("board-6x6");
 const board7x7 = document.getElementById("board-7x7");
 
+const mainInterface = document.getElementById("main-interface");
+
 let b4x4 = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]];
 let s4x4 = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,0]];
 
@@ -27,56 +29,27 @@ let empty = [-1, -1]; // location [row, col] of empty cell
 
 const moveCounter = document.getElementById("moves");
 const totalMoveCounter = document.getElementById("total-moves");
-let moveLock;
+let moveLock = true;
 let moveCount = 0;
 let totalMoveCount = 0;
 
 let timerRunningFlag = false;
 
-// Handle keyboard input
-window.onkeydown = e => {
-    if (moveLock) return;
-
-    if ((e.key == "ArrowUp" || e.key == "w") && empty[0] != boardSize - 1) editBoard("up");
-    else if ((e.key == "ArrowDown" || e.key == "s") && empty[0] != 0) editBoard("down");
-    else if ((e.key == "ArrowLeft" || e.key == "a") && empty[1] != boardSize - 1) editBoard("left");
-    else if ((e.key == "ArrowRight" || e.key == "d") && empty[1] != 0) editBoard("right");
-    else return; 
-
-    if (!timerRunningFlag) {
-       startTimer(); 
-       timerRunningFlag = true;
-       pauseEnabled = true;
-    }
-
-    incrementMoveCount();
-    renderBoard();
-    colorBoard();
-    checkSolved();
-}
-
-const mainInterface = document.getElementById("main-interface");
-const newGame = (startBoard) => {
+function newGame(startBoard) {
     if (board) board.style.display = "none";
     setBoard(startBoard);
-    // shuffleRemaining();
     
     do { shuffleBoard(boardArray); }
     while (!solvable(boardArray.flat()) || checkCorrect() < boardSize);
 
-    // console.log(checkCorrect());
-
+    // Don't reset totalTime or totalMoves since it carries on until going back to menu
     stopTimer();
     timerRunningFlag = false;
     pauseEnabled = false;
     time.innerText = "00:00:00";
-    // Don't reset totalTime, since it carries on to next board
-    // Reset if back to menu (menu.js)
-
     moveLock = false;
     moveCount = 0;
     moveCounter.innerText = "0";
-    // Don't reset totalMoves, since it carries on to next board
 
     renderBoard();
     colorBoard();
